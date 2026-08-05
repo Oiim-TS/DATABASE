0a8ad97d0993f93cd07fcd8de8564ca671d5f451
Oiim-TS <brukid01@gmail.com>
2026-08-05 13:21:08 +0700

    Update SCRIPT_CARD_ONLINE_V1.lua

--- SCRIPT_CARD_ONLINE_V1.lua
+++ SCRIPT_CARD_ONLINE_V1.lua
@@ -12,7 +12,7 @@ USER_NAME = "-"
 USER_EXPIRED = "-"
 USER_LOADED = false
 function Load_User_Info()
-    local url = "https://raw.githubusercontent.com/Oiim-TS/DATABASE/refs/heads/main/LK_CARD"
+    local url = "https://raw.githubusercontent.com/Oiim-TS/DATABASE/refs/heads/main/LICENSE_KEYS_CARD"
 
     local response = gg.makeRequest(url)
 
